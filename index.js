2e3d59ecd3eca53029d9cbec9f07edde7cbdf1c9
Daniel Groves <hello@danielgroves.net>
2019-08-30 12:30:48 +0100

    Fix handling of a URL which has a non-hash component in the place of a hash (#37)
    * Fix handling of a URL which has a non-hash component in the place of a hash
    
    * Refactor to reuse the position of the hash in the file name

--- index.js
+++ index.js
@@ -92,13 +92,15 @@ const staticify = (root, options) => {
 
         const fileName = path.basename(p);
         const fileNameParts = fileName.split('.');
+        const fileNameHashPosition = fileNameParts.length - 2;
+        const fileNameHash = fileNameParts[fileNameHashPosition];
         const re = new RegExp(`^[0-9a-f]{${HASH_LEN}}$`, 'i');
+        const reResult = re.exec(fileNameHash);
 
-        if (fileNameParts.length >= 3 &&
-            fileNameParts[fileNameParts.length - 2].length === HASH_LEN &&
-            re.exec(fileNameParts[fileNameParts.length - 2])[0] === fileNameParts[fileNameParts.length - 2]
+        if (fileNameParts.length >= 3 && fileNameHash.length === HASH_LEN &&
+            (reResult && reResult[0] === fileNameHash)
         ) {
-            const stripped = fileNameParts.slice(0, fileNameParts.length - 2);
+            const stripped = fileNameParts.slice(0, fileNameHashPosition);
 
             stripped.push(fileNameParts[fileNameParts.length - 1]);
 
